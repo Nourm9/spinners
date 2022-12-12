9b00a6fe47c5b23e72289411354ce6fd63c2ccca
Nour Mohamoud <nour.mohamoud9@gmail.com>
2022-12-12 00:41:58 +0000

    update: spinner1.js in unrefactored form; add: spinner2.js refactored form

--- spinner1.js
+++ spinner1.js
@@ -1,8 +1,23 @@
-let spins = ["\r|", "\r/", "\r-", "\r\\", "\r| \n"];
-let interval = 0;
 
-for (let spin of spins) {
-  setTimeout(() => {
-    process.stdout.write(spin);
-  }, interval += 100);
-}
+setTimeout(() => {
+  process.stdout.write("\r|");
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write("\r/");
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write("\r-");
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write("\r\\");
+}, 700);
+setTimeout(() => {
+  process.stdout.write("\r|");
+}, 800);
+
+setTimeout(() => {
+  console.log();
+}, 800);
